fe7d5bf937da3612d8b3096a4c18a8408ced2f46
blueyeshe <akbarimawan18@gmail.com>
2020-06-30 22:26:51 +0700

    Create Activity_tentang.java

---
+++ app/src/main/java/com/bagusf/babeoapss/Activity_tentang.java
@@ -0,0 +1,15 @@
+package com.bagusf.babeoapss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+    }
+}
